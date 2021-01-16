6d6f461caca3192f6ffb486270da25dab2ff7760
josemarmendes <josemarmendes17@gmail.com>
2021-01-16 14:22:49 -0300

    iniciando projeto spe - sistema de ponto eletrônico

---
+++ src/main/java/com/pulse/spe/SpeApiApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.spe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulse/spe/SpeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse.spe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
