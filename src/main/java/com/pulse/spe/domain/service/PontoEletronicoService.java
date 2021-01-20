ead10481072aad0d257e3b0390b48c7b0e37f42b
josemarmendes <josemarmendes17@gmail.com>
2021-01-20 01:20:56 -0300

    removendo códigos inutilizados

--- src/main/java/com/pulse/spe/domain/service/PontoEletronicoService.java
+++ src/main/java/com/pulse/spe/domain/service/PontoEletronicoService.java
@@ -21,46 +21,47 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PontoEletronicoService {
 
-  private final UsuarioRepository usuarioRepository;
-  private final BatidaRepository batidaRepository;
-  private final PontoEletronicoRepository pontoEletronicoRepository;
-  private final PontoEletronicoBusiness pontoEletronicoBusiness;
+	private final UsuarioRepository usuarioRepository;
+	private final BatidaRepository batidaRepository;
+	private final PontoEletronicoRepository pontoEletronicoRepository;
+	private final PontoEletronicoBusiness pontoEletronicoBusiness;
 
+	@Transactional
+	public PontoEletronico registrarBatida(String cpf, LocalDateTime horario) {
+		Usuario usuario = usuarioRepository.findByCpf(cpf);
 
-  @Transactional
-  public PontoEletronico registrarBatida(String cpf, LocalDateTime horario) {
-    Usuario usuario = usuarioRepository.findByCpf(cpf);
-    
+		// por enquanto vem com as batidas ja do banco, se mudar para LAZY, carregar as
+		// batidas na mão
+		PontoEletronico pontoEletronico;
 
-    // por enquanto vem com as batidas ja do banco, se mudar para LAZY, carregar as batidas na mão
-    PontoEletronico pontoEletronico;
-   
-   /* try {
-      pontoEletronico = pontoEletronicoRepository.findByUsuarioAndData(usuario, horario.toLocalDate());
-    
-    } catch (RuntimeException e) {*/ 
-    	// procurar a excecao que esses métodos do pontoEletronicoRepository deve retornar uma  excecao se nao achar dados no banco
-      
-    
-      pontoEletronico = new PontoEletronico();
-      pontoEletronico.setData(horario.toLocalDate());
-      pontoEletronico.setUsuario(usuario);
-      
-      Batida batida = Batida.builder().hora(horario.toLocalTime()).pontoEletronico(pontoEletronico).build();
-      List<Batida> batidas = new ArrayList<>();
-      batidas.add(batida);
-      
-      pontoEletronico.setDeltaMinutos( Long.valueOf(batida.getHora().getMinute()));
-      pontoEletronico.setBatidas(batidas);
-      
-      batidaRepository.save(batida);
-      //pontoEletronico.getBatidas().add(batida);
-      
-    //}
+		/*
+		 * try { pontoEletronico =
+		 * pontoEletronicoRepository.findByUsuarioAndData(usuario,
+		 * horario.toLocalDate());
+		 * 
+		 * } catch (RuntimeException e) {
+		 */
+		// procurar a excecao que esses métodos do pontoEletronicoRepository deve
+		// retornar uma excecao se nao achar dados no banco
 
-    pontoEletronicoBusiness.registrarBatida(pontoEletronico, horario);
+		// }
 
-    return pontoEletronicoRepository.save(pontoEletronico);
-  }
+		pontoEletronico = new PontoEletronico();
+		pontoEletronico.setData(horario.toLocalDate());
+		pontoEletronico.setUsuario(usuario);
+
+		Batida batida = Batida.builder().hora(horario.toLocalTime()).pontoEletronico(pontoEletronico).build();
+		List<Batida> batidas = new ArrayList<>();
+		batidas.add(batida);
+
+		pontoEletronico.setDeltaMinutos(Long.valueOf(batida.getHora().getMinute()));
+		pontoEletronico.setBatidas(batidas);
+
+		batidaRepository.save(batida);
+
+		pontoEletronicoBusiness.registrarBatida(pontoEletronico, horario);
+
+		return pontoEletronicoRepository.save(pontoEletronico);
+	}
 
 }
