891d26473e1a43917c71c1e632b5847421233d39
jmneto <jmneto@jmneto-HP-ProDesk-400-G4-SFF>
2021-01-20 17:09:54 -0300

    adicionando token para acesso as rotas no swagger

--- src/main/java/com/pulse/spe/api/SwaggerConfig.java
+++ src/main/java/com/pulse/spe/api/SwaggerConfig.java
@@ -1,10 +1,17 @@
 package com.pulse.spe.api;
+
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.pulse.spe.domain.model.Usuario;
+
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -14,25 +21,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.ant("/**"))
-                .build()
-                .apiInfo(apiInfo());
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.pulse.spe.api.controller"))
+				.paths(PathSelectors.ant("/**")).build()
+				.ignoredParameterTypes(Usuario.class)
+				.globalOperationParameters(
+					Arrays.asList(
+						new ParameterBuilder()
+						.name("Authorization")
+						.description("Token JWT")
+						.modelRef(new ModelRef("string"))
+						.parameterType("header").required(false).build()
+					)
+				);
+	}
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .contact(new Contact("Josemar Mendes", "", ""))
-                .title("SPE-Sistema de Ponto eletrônico")
-                .description("Documentação API Spe - Sistema de ponto eletrônico")
-                .license("Apache Licence Version 2.0")
-                .licenseUrl("https://apache.org")
-                .version("1.0")
-                .build();
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().contact(new Contact("Josemar Mendes", "", ""))
+				.title("SPE-Sistema de Ponto eletrônico")
+				.description("Documentação API Spe - Sistema de ponto eletrônico")
+				.license("Apache Licence Version 2.0")
+				.licenseUrl("https://apache.org")
+				.version("1.0").build();
 
-    }
+	}
 }
