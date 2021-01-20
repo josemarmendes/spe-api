f7e4a56070bc56c177ea0a372068abc80e41dd1a
jmneto <jmneto@jmneto-HP-ProDesk-400-G4-SFF>
2021-01-20 17:56:37 -0300

    classe service utilizada para autenticação

---
+++ src/main/java/com/pulse/spe/domain/service/AutenticacaoService.java
@@ -0,0 +1,23 @@
+package com.pulse.spe.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.pulse.spe.domain.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+	
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String cpf) throws UsernameNotFoundException {
+		return usuarioRepository.findByCpf(cpf);
+	}
+	
+}
