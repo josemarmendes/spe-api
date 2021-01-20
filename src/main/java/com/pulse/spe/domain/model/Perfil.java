7183bc56764890d190d8237afcf92c054b5549d6
jmneto <jmneto@jmneto-HP-ProDesk-400-G4-SFF>
2021-01-20 17:15:52 -0300

    adicionando classe usada para os perfis administrador e colaborador que terão acesso ao sistema

---
+++ src/main/java/com/pulse/spe/domain/model/Perfil.java
@@ -0,0 +1,36 @@
+package com.pulse.spe.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.Data;
+
+@Entity
+@Table(schema = "spe" , name= "perfil")
+@Data
+@SequenceGenerator(name = "spe.perfil_id_seq", sequenceName = "spe.perfil_id_seq", allocationSize = 1)
+public class Perfil implements GrantedAuthority{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="perfil_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "spe.perfil_id_seq")
+	private Long perfilId;
+	
+	private String nome;
+
+	@Override
+	public String getAuthority() {
+		
+		return this.nome;
+	}
+
+}
